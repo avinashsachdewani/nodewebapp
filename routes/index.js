50626f51e2804e76598d2ea0632c3d13d3ecf56f
Avinash <avinash.sachdewani@motifworks.com>
2015-12-10 16:09:12 +0530

    First commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+
+// only needed if calling the rest api from this file (accounts route)
+var rest = rest = require('./../rest.js');
+
+/*
+ * GET home page.
+ */
+exports.index = function (req, res){
+    console.log("check");
+  res.render('index', { title: 'Salesforce.com Node.js REST Demo' })
+};
+
+/*
+ * GET list of accounts - for larger apps, you may want to separate
+ * code into different routes for ease of maintenance and logic. 
+ * Prevents app.js from becoming huge!
+ */
+exports.accounts = function (req, res){
+    console.log("check");
+	rest.api(req).query("select id, name from account limit 10", function(data) {
+		res.render("accounts", { title: 'Accounts', data: data, user: req.session.identity } );
+	});
+};
