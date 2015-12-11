a39c3a4f7b4b6c1a5605b63418cd4525035037cd
Avinash <avinash.sachdewani@motifworks.com>
2015-12-11 19:46:57 +0530

    remove proxy url

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 var soql = document.getElementById("soql");
 var result = document.getElementById("result");
-var clientId = 'YOUR_CONSUMER_KEY_HERE',
-    loginUrl = 'https://login.salesforce.com/',
-    redirectURI = 'http://localhost:3000/oauthcallback.html',
-    proxyURL = 'http://localhost:3000/proxy/',
-    client = new forcetk.Client(clientId, loginUrl, proxyURL);
+var client = new forcetk.Client(clientId, loginUrl, proxyURL);
 function loginDialogCallback(response) {
     if (response && response.access_token) {
         client.setSessionToken(response.access_token, null, response.instance_url);
